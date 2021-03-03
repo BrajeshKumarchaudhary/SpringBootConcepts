05931c131b4334ad50e5bada342cf588e3edf932
Brajesh <bk04031997@gmail.com>
2021-03-03 10:40:28 +0530

    Cross origin Implementation

--- SpringBoot-Mockito/src/test/java/com/app/controller/AccountTestController.java
+++ SpringBoot-Mockito/src/test/java/com/app/controller/AccountTestController.java
@@ -1,8 +1,11 @@
 package com.app.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
+import java.util.LinkedList;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,17 +13,68 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.app.model.Account;
+import com.app.service.AccountCalculationService;
 import com.app.service.AccountService;
+import com.app.service.LoggerService;
+import com.app.service.UserService;
 
+/*
+ * by Annoating this Spring create One context for Whole Classes.and create bean of whole class.
+ */
 @SpringBootTest
+/*
+ * //By this Spring context register only specified class. create only these beans.
+ */
+//@ContextConfiguration(classes = {AccountService.class,AccountCalculationService.class,UserService.class}) 
 @RunWith(SpringRunner.class)
+
 public class AccountTestController {
 
 	@Autowired
 	AccountService accountService;
+	
+	/*
+	 * When Any Service is calling Any other Service one Way to injects by @Before and init function.like this.
+	 */
+//	@Before
+//	public void init() {
+//		AccountCalculationService service1=new AccountCalculationService();
+//		 accountService.setAccountCalService(service1);
+//	}
+	
+	
+	/*
+	 * When any service interacting any IO operation like DataBase.Reading Value from dataBase then use @MockBean and initalized the service.
+	 * By Using this it is return dummy response  from each function and avoid to calling dataBase.
+	 */
+	@MockBean
+	UserService userService;
+	
+	
+	/*
+	 * When Any service need to avoid calling any function then used @SpyBean
+	 */
+	
+	@SpyBean
+	LoggerService loggerService;
+	
+	
+	
+	@Test
+	public void integrationTesting() {
+		LinkedList<Integer> l=new LinkedList<>();
+		l.add(200);
+		Mockito.when(userService.getUserId()).thenReturn(l);
+		LinkedList<Integer> l1=userService.getUserId();
+//		assertThat(l1).isEmpty();
+		
+	}
 
 	@Test
 	@DisplayName("This Is Used for Testing of Account getting Exacly or not")
